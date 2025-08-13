1c550c775e53eb52bb63637c6d5c5eb8ef28d651
zacexdd <adammzandi67@gmail.com>
2025-08-13 12:10:45 +0100

    Add files via upload

--- gag.lua
+++ gag.lua
@@ -3,7 +3,7 @@ if game.PlaceId == 126884695634066 then
     local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
     local Window = Rayfield:CreateWindow({
-        Name = "Pet and seed spawner script",
+        Name = "Dark Spawner by Zace",
         Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
         LoadingTitle = "Zace hub",
         LoadingSubtitle = "by Zace",
